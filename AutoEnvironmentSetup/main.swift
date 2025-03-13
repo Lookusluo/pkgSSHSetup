cd82ed2e0146f5d17b413407a000a69863a3407f
YazhongLuo <comeluo@hotmail.com>
2025-03-13 15:16:58 +0800

    Update ssh adding method

--- AutoEnvironmentSetup/main.swift
+++ AutoEnvironmentSetup/main.swift
@@ -94,27 +94,25 @@ func installPackages() {
 }
 
 func setupSSHKey() {
-    let fileManager = FileManager.default
     
-    if !fileManager.fileExists(atPath: sshKeyPath) {
-        print("Generating SSH key...")
-        let process = Process()
-        process.executableURL = URL(fileURLWithPath: "/usr/bin/ssh-keygen")
-        process.arguments = ["-t", "ed25519", "-f", sshKeyPath, "-N", ""]
-        
-        process.standardInput = FileHandle.standardInput
-        process.standardOutput = FileHandle.standardOutput
-        process.standardError = FileHandle.standardError
-        
-        try? process.run()
-        process.waitUntilExit()
-        
-        let sshAddProcess = Process()
-        sshAddProcess.executableURL = URL(fileURLWithPath: "/usr/bin/ssh-add")
-        sshAddProcess.arguments = [sshKeyPath.replacingOccurrences(of: ".pub", with: "")]
-        try? process.run()
-        process.waitUntilExit()
-    }
+    print("Generating SSH key...")
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/ssh-keygen")
+    process.arguments = ["-t", "ed25519", "-f", sshKeyPath, "-N", ""]
+    
+    process.standardInput = FileHandle.standardInput
+    process.standardOutput = FileHandle.standardOutput
+    process.standardError = FileHandle.standardError
+    
+    try? process.run()
+    process.waitUntilExit()
+    
+    let sshAddProcess = Process()
+    sshAddProcess.executableURL = URL(fileURLWithPath: "/usr/bin/ssh-add")
+    sshAddProcess.arguments = [sshKeyPath.replacingOccurrences(of: ".pub", with: "")]
+    try? process.run()
+    process.waitUntilExit()
+    
 }
 
 // Configure GitHub CLI authentication
@@ -125,7 +123,7 @@ func configureGitHubCLI() {
     let process = Process()
     process.executableURL = URL(fileURLWithPath: "/bin/bash")
     let command = """
-    osascript -e 'tell application "Terminal" to do script "gh auth login -h \(domainURL) -p https"'
+    osascript -e 'tell application "Terminal" to do script "gh auth login -h \(domainURL) -p ssh'
     """
     process.arguments = ["-c", command]
     
